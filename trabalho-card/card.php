90e75db6928bf66b0d6e89583a60bbe52a46a505
dan <134729425+danxzxz@users.noreply.github.com>
2025-05-09 15:38:33 -0300

    Add files via upload

--- trabalho-card/card.php
+++ trabalho-card/card.php
@@ -1,31 +1,32 @@
 <?php
 
 $nome = $_POST['nome']; 
-$diretor = $_POST['diretor']; 
+$autor = $_POST['autor']; 
 $ano = $_POST['ano']; 
-$link = $_POST['link']; 
+$genero = $_POST['genero'];
+$sinopse = $_POST['sinopse'];
+$link = $_POST['link'];
 
-
-echo
-"
+echo "
 <!DOCTYPE html>
 <html lang='en'>
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <link rel='stylesheet' href='css/estilo_card.css'>
-    <title>Document</title>
+    <title>Livro Cadastrado</title>
 </head>
 <body>
     <div class='card'>
         <h2>$nome</h2>
-        <p>Diretor: $diretor</p>
-        <p>Ano: $ano</p>
-        <img src='$link' alt='Imagem do Filme'/>
+        <p><strong>Autor:</strong> $autor</p>
+        <p><strong>Ano:</strong> $ano</p>
+        <p><strong>Gênero:</strong> $genero</p>
+        <p><strong>Sinopse:</strong><br>$sinopse</p>
+        <img src='$link' alt='Capa do Livro' />
     </div>
 
+    <a href='formulario.php'>Cadastrar outro livro</a>
 </body>
 </html>";
 
-echo "<br><br>";
-echo "<a href='formulario.php'>Cadastrar outro filme</a>";
--- trabalho-card/formulario.php
+++ trabalho-card/formulario.php
@@ -4,21 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo_form.css">
-    <title>Document</title>
+    <title>Cadastro de Livro</title>
 </head>
 
 <body>
-    <h2>Dados do Filme</h2> 
+    <h2>Cadastro de Livro</h2> 
     <form method="POST" action="card.php">
-        <input name="nome" placeholder="Informe o nome"/>
+        <input name="nome" placeholder="Título do Livro" />
         <br>
-        <input name="diretor" placeholder="Informe o diretor" />
+        <input name="autor" placeholder="Autor" />
         <br>
-        <input name="ano" placeholder="Informe o ano" />
+        <input name="ano" placeholder="Ano de Publicação" />
         <br>
-        <input name="link" placeholder="Informe a foto (link)" />
+        <input name="genero" placeholder="Gênero Literário" />
+        <br>
+        <textarea name="sinopse" placeholder="Sinopse" rows="4" cols="30" style="margin: 10px 0; padding: 10px; border-radius: 5px;"></textarea>
+        <br>
+        <input name="link" placeholder="URL da capa do livro" />
         <br><br>
-        <button type="submit">Enviar</button>
+        <button type="submit">Cadastrar</button>
     </form>
 </body>
-
+</html>
