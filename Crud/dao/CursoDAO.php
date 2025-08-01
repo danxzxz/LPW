6944bc8191eb5750fb21462f1c0bf515487c64ae
dan <134729425+danxzxz@users.noreply.github.com>
2025-08-01 15:43:16 -0300

    Add files via upload

---
+++ Crud/dao/CursoDAO.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once(__DIR__ . "/../util/Connection.php");
+require_once(__DIR__ . "/../model/Curso.php");
+
+class CursoDAO {
+
+    private PDO $conexao;
+
+    public function __construct() {
+        $this->conexao = Connection::getConnection();        
+    }
+
+    public function listar() {
+        $sql = "SELECT * FROM cursos ORDER BY nome";
+        $stm = $this->conexao->prepare($sql);
+        $stm->execute();
+        $result = $stm->fetchAll();
+
+        return $this->map($result);
+    }
+
+    private function map(array $result) {
+        $cursos = array();
+        foreach($result as $r) {  
+            $curso = new Curso();
+            $curso->setId($r["id"]);
+            $curso->setNome($r["nome"]);
+            $curso->setTurno($r["turno"]);
+
+
+            array_push($cursos, $curso);
+        }
+        return $cursos;
+    }
+
+}
